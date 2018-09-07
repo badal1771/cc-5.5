5c40d575492cc5eb18eeefc373a7d568bd1f5191
badal1771 <42205975+badal1771@users.noreply.github.com>
2018-09-07 10:53:50 -0400

    Add files via upload

---
+++ Lab5.5_q1.cpp
@@ -0,0 +1,25 @@
+/*drawing a star cube
+*****
+*****
+*****
+*****
+***** 
+*/
+//library
+#include<iostream>
+using namespace std;
+//main function
+int main(){
+int n = 5, i = 0 , j = 0;
+//loop
+for(i = 0 ; i < n; i++ )
+{
+//writing loop in loop
+for(j= 0 ;j< n ; j++ )
+{cout<<"*";
+}
+cout<<endl;
+}
+return 0;
+}
+
